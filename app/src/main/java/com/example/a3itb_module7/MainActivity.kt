359942e712c045178419b3dfeb505db67fa326e4
Elisha Amon Garma <elishaamon.garma.cics@ust.edu.ph>
2023-10-27 10:00:09 +0800

    Started working on hashmaps

--- app/src/main/java/com/example/a3itb_module7/MainActivity.kt
+++ app/src/main/java/com/example/a3itb_module7/MainActivity.kt
@@ -16,12 +16,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var hashWords : HashMap<Int, String> = HashMap()
+        hashWords.put(0 , "Zero")
+        hashWords.put(1, "One")
+        hashWords.put(2, "Two")
+        hashWords.put(3, "Three")
+
+        var random = Random
+        var rangeRandom = random.nextInt(4)
+
+        var hashMain : HashMap<Int, ArrayList<RandomValue>>
+
+        var hash : HashMap<String, String> = HashMap()
+        hash.put("One", "I am number one")
+        hash.get("One")
+        Log.i("info_garma", hash.get("One").toString())
+
         /*range = 30
         var randomNum = Random
         var randomfunc = randomNum.nextInt(10)
         randomfunc++
 
-        Log.i("info_garma", randomfunc.toString() )*/
+        Log.i("info_garma", randomfunc.toString() )
         var randClass = RandomClass()
         var textRandom : TextView = findViewById(R.id.txtRandom)
         var editRange : EditText = findViewById(R.id.edtRange)
@@ -30,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         buttonRandom.setOnClickListener {
             var intValue = parseInt(editRange.text.toString())
             textRandom.setText(randClass.Randomizer(intValue).toString())
-        }
+
+        }*/
     }
 }
---
+++ app/src/main/java/com/example/a3itb_module7/RandomValue.kt
@@ -0,0 +1,5 @@
+package com.example.a3itb_module7
+
+class RandomValue (var numWords : String,
+                   var numInt: Int,
+                   var numBoolean : Boolean)
